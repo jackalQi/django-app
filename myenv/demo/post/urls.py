c199160951ca0a550486aacd2c01001f917d4d3e
jackalQi <32143278+jackalQi@users.noreply.github.com>
2018-07-22 16:24:14 -0400

    version 2

--- myenv/demo/post/urls.py
+++ myenv/demo/post/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.get_posts, name='posts_list'),
     path('post/<int:pk>/', views.get_post, name='post_detail'),
+    path('new/', views.new_post, name='new_post'),
+    path('edit/<int:pk>', views.edit_post, name='edit_post'),
 
 ]
--- myenv/demo/post/views.py
+++ myenv/demo/post/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from . models import Post
+from .forms import PostFrom
+from django.utils import timezone
 # Create your views here.
 
 
@@ -11,6 +13,42 @@ def get_posts(request):
 def get_post(request, pk):
     post = get_object_or_404(Post, id=pk)
     return render(request, 'post_detail.html', {"post": post})
+
+
+def new_post(request):
+    if request.method == "POST":
+        form = PostFrom(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.post_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostFrom()
+    return render(request, 'new_post.html', {"form": form})
+
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostFrom(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.post_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostFrom(instance=post)
+        return render(request, 'edit_post.html', {"form": form})
+
+
+def delete_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+
+    return redirect('post_list')
+
+
 
 
 
